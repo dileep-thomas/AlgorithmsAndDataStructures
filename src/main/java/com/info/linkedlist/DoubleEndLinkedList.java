39eb299698055b0fbb52a0f7f0a4b3a7ba74b816
dileep-thomas <dileep_thomas@apple.com>
2019-05-31 19:59:15 -0700

    Double ended linked list

--- src/main/java/com/info/linkedlist/DoubleEndLinkedList.java
+++ src/main/java/com/info/linkedlist/DoubleEndLinkedList.java
@@ -1,5 +1,48 @@
 package com.info.linkedlist;
 
+import com.info.common.Node;
+
+/**
+ * 
+ * @author dileep-thomas
+ *
+ */
 public class DoubleEndLinkedList {
+
+	private Node head;
+	private Node tail;
+
+	public Node insertAtfirst(String key) {
+
+		Node node = new Node(key);
+
+		if (isEmpty()) {
+			tail = node;
+		} else {
+			node.setNext(head);
+
+		}
+		head = node;
+		return node;
+
+	}
+
+	public Node insertAtEnd(String key) {
+		Node node = new Node(key);
+
+		if (isEmpty()) {
+			head = node;
+		} else {
+			tail.setNext(node);
+		}
+		tail = node;
+		return node;
+
+	}
+
+	private boolean isEmpty() {
+		return head == null;
+
+	}
 
 }
